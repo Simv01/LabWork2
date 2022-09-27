753a55669bb41f5bc892fc74422713a172bdb1c8
Боднар Михайло Володимирович <mihajlobodnar8@gmail.com>
2022-09-27 15:07:28 +0300

    Labwork2

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	int x,y,z;
+	cout << "z=x*y" << endl;
+	cout << "input x"<< endl;
+	cin >> x;
+	cout << "input y" << endl;
+	cin >> y;
+	z = x * y;
+	cout<< "z=" << z << endl;
+	cout << "Press any key"<< endl;
+
+	
+	
+	_getch();
+}
